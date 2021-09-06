ef7c734f961c1a520b3b879e2573dbf12f56e381
greeenly <greeenly.dev@gmail.com>
2021-09-06 13:35:51 +0900

    feat: Value 어노테이션을 이용한 환경변수 값 가져오기

--- user-service/src/main/java/me/greeenly/userservice/controller/UserController.java
+++ user-service/src/main/java/me/greeenly/userservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package me.greeenly.userservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.greeenly.userservice.vo.Greeting;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,9 @@ public class UserController {
     // Env 객체를 사용하여 환경설정 파일의 값 가져오기
     private final Environment env;
 
+    // Value 어노테이션을 활용한 환경설정 파일의 값 가져오기
+    private final Greeting greeting;
+
     // 상태체크
     @GetMapping("/health_check")
     public String status() {
@@ -22,7 +26,8 @@ public class UserController {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return env.getProperty("greeting.message");
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
     }
 
 }
