32d59de5b5ad7d20d3e96ce662f4a033011e1677
greeenly <greeenly.dev@gmail.com>
2021-09-06 13:15:26 +0900

    feat: Enviroment 객체를 이용한 환경변수 값 가져오기

--- user-service/src/main/java/me/greeenly/userservice/controller/UserController.java
+++ user-service/src/main/java/me/greeenly/userservice/controller/UserController.java
@@ -1,17 +1,28 @@
 package me.greeenly.userservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class UserController {
+
+    // Env 객체를 사용하여 환경설정 파일의 값 가져오기
+    private final Environment env;
 
     // 상태체크
     @GetMapping("/health_check")
     public String status() {
         return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return env.getProperty("greeting.message");
     }
 
 }
